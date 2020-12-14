0aa9555041fb5f2021be5d387be85ccb63ee15e6
Ado Pabianko <adopabianko@gmail.com>
2020-12-14 20:41:08 +0700

    variable types

---
+++ variable_types/dictionary.py
@@ -0,0 +1,11 @@
+dict = {}
+dict['one'] = "This is one"
+dict[2] = "This is two"
+
+tinydict = {'name': 'john', 'code': 6734, 'dep': 'sales'}
+
+print (dict['one']) # Prints value for 'one' key
+print (dict[2]) # Prints value for 2 key
+print (tinydict) # Prints complete didctionary
+print (tinydict.keys()) # Prints all the keys
+print (tinydict.values()) # Prints all the values
---
+++ variable_types/lists.py
@@ -0,0 +1,9 @@
+list = ['abcd', 786, 2.23, 'john', 70.2]
+tinylist = [123, 'john']
+
+print(list) # Prints complete list
+print(list[0]) # Prints first element of the list
+print(list[1:3]) # Prints elements starting from 2n till 3rd
+print(list[2:]) # Prints elements starting from 3rd element
+print(tinylist * 2) # Prints list two times
+print(list + tinylist) # Prints concatenated lists
